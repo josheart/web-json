5289cdd650d880c83574c8404cfdc25cba77920e
josheart <cesurthesdet@gmail.com>
2019-12-15 00:11:12 -0600

    initial commit

---
+++ src/main/java/com/inss/webjson/WebJsonApplication.java
@@ -0,0 +1,13 @@
+package com.inss.webjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inss/webjson/WebJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inss.webjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
