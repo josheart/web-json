ddbe3871b5f7cf251b7943117e788bde5e9a8c72
josheart <cesurthesdet@gmail.com>
2019-12-17 00:04:58 -0600

    first mvc try with thymeleaf.

---
+++ src/main/java/com/inss/webjson/HomeController.java
@@ -0,0 +1,13 @@
+package com.inss.webjson;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
