9c68d8005fb6c9564250789c7453849ec7d67832
gx <fairwinds0099@gmail.com>
2020-01-06 19:09:30 -0600

    added link moved controller class in the controller package

--- src/main/java/com/inss/webjson/controller/HomeController.java
+++ src/main/java/com/inss/webjson/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.inss.webjson;
+package com.inss.webjson.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +9,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/gxk")
+    public String gx() {
+        return "gx";
     }
 }
--- src/test/java/com/inss/webjson/HomeControllerTest.java
+++ src/test/java/com/inss/webjson/HomeControllerTest.java
@@ -1,5 +1,6 @@
 package com.inss.webjson;
 
+import com.inss.webjson.controller.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
